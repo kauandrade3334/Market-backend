4544814dbba26b06d562d9fd1f14aae215264ac5
kauandrade3334 <kauandradepessoal@gmail.com>
2026-08-18 16:00:13 -0300

    build: project started #1

---
+++ src/main/java/com/market/Market_backend/MarketBackendApplication.java
@@ -0,0 +1,13 @@
+package com.market.Market_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketBackendApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(MarketBackendApplication.class, args);
+	}
+
+}
