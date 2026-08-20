9d4b886ceb70133058e2bdab2d63c83d3c6548bf
kauandrade3334 <kauandradepessoal@gmail.com>
2026-08-20 12:50:21 -0300

    feat: add initial repositories #3

---
+++ src/main/java/com/market/Market_backend/repository/UsersRepository.java
@@ -0,0 +1,9 @@
+package com.market.Market_backend.repository;
+
+import com.market.Market_backend.entity.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsersRepository extends JpaRepository<Users, String> {
+}
